6b02bf880fba0ef64668e2fccbfe896e046c111a
Mohamed tefili <m.ecomplus1@gmail.com>
2023-05-23 15:09:52 +0200

    add eventListener et console.log

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,17 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+// Sélectionne les éléments des flèches
+let leftArrow = document.getElementById("left");
+let rightArrow = document.getElementById("right");
+
+// Ajoute un gestionnaire d'événements pour les flèches gauche et droite
+leftArrow.addEventListener("click", function() {
+  // Phrase qui apparaît lors du clic sur la flèche gauche
+  console.log("Flèche gauche");
+});
+
+rightArrow.addEventListener("click", function() {
+  // Phrase qui apparaît lors du clic sur la flèche droite
+  console.log("Flèche droite");
+});
